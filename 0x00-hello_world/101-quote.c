8736c5d89a6743485e0efac74d456fbfef199577
hakima7 <hakimakarboub@gmail.com>
2023-06-15 16:59:05 -0700

    tasks 100 and 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 1 (success)
- */
+* main - Entry point
+*
+* Return: Always 1 (success)
+*/
 int main(void)
 {
-    ssize_t len;
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    len = write(2, message, 59);
-    (void)len;  /* To silence unused variable warning */
-    return (1);
+	ssize_t len;
+	char *message="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	len=write(2, message, 59);
+	(void)len;/* To silence unused variable warning */
+	return (1);
 }
