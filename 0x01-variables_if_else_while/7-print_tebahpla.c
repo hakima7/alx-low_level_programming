8b8634799f2de7d5471d134d9518ad2ff63e6e43
hakima7 <hakimakarboub@gmail.com>
2023-06-16 13:35:57 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int low;
+
+	for (low = 'z'; low <= 'a'; low--)
+	putchar(low)
+	putchar('\n)
+
+	return (0);
+
+}
