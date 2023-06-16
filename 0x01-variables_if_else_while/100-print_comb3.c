dc6464f9aa08b6d24ae042a874f5c97f532b0632
hakima7 <hakimakarboub@gmail.com>
2023-06-16 15:14:20 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -25,9 +25,11 @@ int main(void)
 	{
 	putchar(d);
 	putchar(p);
-	if (d != '8' && p != '9')
-	putchar(',');
-	putchar(' ');
+	if (d != '8' || p != '9')
+       	{
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	}
 	}
