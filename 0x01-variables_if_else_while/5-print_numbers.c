6211a880b161bb48ca11bf2c376d55c7e8da5919
hakima7 <hakimakarboub@gmail.com>
2023-06-16 13:09:28 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -16,8 +16,9 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number < 10; nimber++)
+	for (number = 0; number < 10; number++)
 	putchar(number + '0');
-	putchar(('\n');
+	putchar('\n');
 			return (0);
+
 }
