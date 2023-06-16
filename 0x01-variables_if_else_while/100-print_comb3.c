33eba8826c0ce0bc3b3b3145bfdb2ed3d36619af
hakima7 <hakimakarboub@gmail.com>
2023-06-16 16:47:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,8 +15,10 @@ int main(void)
 
 	int d, p;
 
-	for (d = '0'; d < '8'; d++)
-	for (p = d + 1; p < '9'; p++) 
+	for (d = '0'; d < '9'; d++)	
+	{
+		
+	for (p = d + 1; p < '9'; p++)
 	{
 	if (p != d)
 	{
@@ -26,6 +28,7 @@ int main(void)
 	continue;
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	putchar('\n');
