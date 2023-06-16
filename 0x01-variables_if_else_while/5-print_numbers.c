5cef1483c574686e717dacba72916761c64bafee
hakima7 <hakimakarboub@gmail.com>
2023-06-16 13:03:58 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int number;
+
+	for (number = 0; number < 10; nimber++)
+	putchar(number + '0');
+	putchar(('\n');
+			return (0);
+}
