c799954dd5d7be1a04579959e3f240455f4d6032
hakima7 <hakimakarboub@gmail.com>
2023-06-21 05:30:41 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,13 @@
 int print_last_digit(int n)
 
 {
-	int last;
+	int last_digit;
 
-	last = n % 10;
-	if (last < 0)
+	last_digit = n % 10;
+	if (last_digit < 0)
 	{
-	last = last * -1;
+	last = last_digit * -1;
 	}
-	_putchar(last + '0');
-	return (last);
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
