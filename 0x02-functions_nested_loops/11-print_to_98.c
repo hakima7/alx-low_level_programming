9d82a787a5e2e832c6785dc50574026d4e997ad1
hakima7 <hakimakarboub@gmail.com>
2023-06-21 11:40:34 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,33 +4,44 @@
 
 
 /**
- * print_to_98 - Prints all natiral numbers fromn to 98
+ * print_to_98 - Prints all natiral numbers from to 98
  * @c: The number to start printing from
- * Return: Alwayse 0
+ * Return: Always 0
  */
+
 void Print_to_98(int n)
+
 {
 	if (n <= 98) 
 	{
         for (; n <= 98; n++) 
 	{
-		printf("%d", n);
-		if (n != 98) 
-		{
-                printf(", ");
-		}
+	if (n == 98)
+	{
+	printf("%d", n);
+	printf("\n");
+	break;
+	}
+	else
+	{
+        printf("%d, ", n);
+	}
 	}
 	}
 	else
 	{
         for (; n >= 98; n--)
-	       }
-		printf("%d", n);
-		if (n != 98)
-		{
-                printf(", ");
-		}
-	}
-	}
+	{
+	if (n == 98)
+	{
+	printf("%d", n);
 	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", n);
+	}
+	}
+	}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,4 @@ void jack_bauer(void);
 void times_tabler(void);
 int add(int, int);
 void print_times_table(int n);
-int print_to_98(int n);
+void print_to_98(int n);
