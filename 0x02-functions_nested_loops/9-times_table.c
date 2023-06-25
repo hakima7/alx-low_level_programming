bbafa873bd023cd868c767b79827f835fb561762
hakima7 <hakimakarboub@gmail.com>
2023-06-25 14:21:54 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,25 +14,26 @@ void times_table(void)
 	{
 	for (j = 0; j < 10; j++)
 	{
-		k = j * i
+		k = j * i;
 
 		if (j != 0)
 		{
 			_putchar(',');
 			_putchar(' ');
+
 			if (k < 10)
 				_putchar(' ');
-			}
 			else
 				_putchar((k / 10) + '0');
 
 			_putchar((k % 10) + '0');
-			}
-				else
-				{
-					_putchar('0');
-				}
-			}
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+
 	_putchar('\n');
 	}
 }
