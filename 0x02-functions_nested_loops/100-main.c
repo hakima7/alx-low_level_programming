682971290daa87674d67cbbcccf596a51257a9d3
hakima7 <hakimakarboub@gmail.com>
2023-06-25 16:13:49 -0700

    100-times_table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int main(void)
+{
+    print_times_table(3);
+    _putchar('\n');
+
+    print_times_table(5);
+    _putchar('\n');
+
+    print_times_table(98);
+    _putchar('\n');
+
+    print_times_table(12);
+    _putchar('\n');
+
+    return (0);
+}
