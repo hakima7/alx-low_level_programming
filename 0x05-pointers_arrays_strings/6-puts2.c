ab587bd22aff9e0b1da2d9b1d23b8c4491cf085d
hakima7 <hakimakarboub@gmail.com>
2023-06-27 11:49:18 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,27 +9,10 @@
 void puts2(char *str)
 {
 	int i;
-
+	
 	for (i = 0; str[i] != '\0'; i += 2)
 	{
 		putchar(str[i]);
 	}
-	putchar('\n');;
-}
-
-int main(void)
-{
-    char str[] = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.";
-    puts2(str);
-
-    return 0;
-}
-}
-
-int main(void)
-{
-    char str[] = "Lrmismdlrstae,cnettraiicn lt e oeumdtmo niiutu aoee ooemgaaiu.U nma ii eim usnsrdeectto lac aoi iiu lqi xe omd osqa.Di ueiuedlri erhnei nvlpaevltes ilmdlr ufga ul aitr xetu itocea uiaa o riet uti up u fii eeutmli nmi s aou.";
-    puts2(str);
-
-    return 0;
+	putchar('\n');
 }
