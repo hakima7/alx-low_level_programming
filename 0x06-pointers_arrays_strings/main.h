c55583e3686dfe39704e12d892078b17f1b54e03
hakima7 <hakimakarboub@gmail.com>
2023-06-29 10:18:33 -0700

    4-rev_array

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *_strncpy(char *dest, char *src, int n);
+void print_array(int *a, int n);
 
 #endif /* MAIN_H */
