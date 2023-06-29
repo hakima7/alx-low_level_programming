291114b18c119278fd389c162430c8083c3326c4
hakima7 <hakimakarboub@gmail.com>
2023-06-29 14:55:50 -0700

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - encodes a string using ROT13 cipher
+ * rot13 - encodes rot13
  * @str: The string to encode
  * Return: A pointer to the resulting encoded string
  */
+
 char *rot13(char *str)
 {
 	int i, j;
 	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char *rot13_chars = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; str[i]; i++)
+	for (i = 0; str[i] !='\0'; i++)
 	{
-		for (j = 0; letters[j]; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (str[i] == letters[j])
 			{
 				str[i] = rot13_chars[j];
+				break;
 			}
 		}
 	}
