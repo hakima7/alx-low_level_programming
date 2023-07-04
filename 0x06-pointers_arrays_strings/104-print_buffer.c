552cfc90e6eddd8ef8753c3ad7d9f3826dee64f3
hakima7 <hakimakarboub@gmail.com>
2023-07-03 17:00:14 -0700

    104-buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,49 +1,55 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * print_buffer - Prints buffer.
- * @b: buffer
- * @size: size
+ * print_buffer - Prints the content of a buffer.
+ * @b: The buffer to print.
+ * @size: The size of the buffer.
+ *
+ * The function prints the content of size bytes of the buffer pointed by b.
+ * It prints 10 bytes per line.
+ * If size is 0 or less, it prints a new line only.
  */
 
 void print_buffer(char *b, int size)
 {
-    int o, i, j;
-
-    o = 0;
+    int i, j;
 
     if (size <= 0)
     {
         printf("\n");
         return;
     }
-    while (o < size)
-    {
-	    j = size - o < 10 ? size - o : 10;
-	    printf("%08x: ", o);
-	    for (i = 0; i < 10; i++)
-	    {
-		    if (i < j)
-			    printf("%02x", *(b + o + i));
-		    else
-			    printf(" ");
-		    if (i % 2)
-		    {
-			    printf(" ");
-		    }
-	    }
-	    for (i = 0; i < j; i++)
-	    {
-		    int c = *(b + o + i);
 
-		    if (c < 32 || c > 132)
-		    {
-			    c = '.';
-		    }
-		    printf("%c", c);
-	    }
-	    printf("\n");
-	    o += 10;
+    for (i = 0; i < size; i += 10)
+    {
+        printf("%08x: ", i);
+
+        for (j = i; j < i + 10; j++)
+        {
+            if (j < size)
+                printf("%02x", b[j]);
+            else
+                printf("  ");
+
+            if (j % 2 == 1)
+                printf(" ");
+        }
+
+        for (j = i; j < i + 10; j++)
+        {
+            if (j < size)
+            {
+                if (b[j] >= 32 && b[j] <= 126)
+                    printf("%c", b[j]);
+                else
+                    printf(".");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
     }
 }
