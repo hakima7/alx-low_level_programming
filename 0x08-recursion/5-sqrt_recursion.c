c494f45691d797c4bad5c35ebdc0168a46f2b5ce
hakima7 <hakimakarboub@gmail.com>
2023-07-05 11:05:35 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,7 @@ int _sqrt_recursion(int n)
  * @n: number to calculate the square root
  * @i: iterate number
  *
- * Retun: the natural square root
+ * Return: the natural square root
  */
 
 int _sqrt(int n, int i)
