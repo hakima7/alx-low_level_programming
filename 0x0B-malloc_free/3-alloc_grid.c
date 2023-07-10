6a01f510ca310bd0561af712cd9e03987840e614
hakima7 <hakimakarboub@gmail.com>
2023-07-10 14:44:32 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * alloc_grid - ...
@@ -11,40 +12,40 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j, k, l;
-	int **a;
+        int i, j, k, l;
+        int **a;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
+        if (width <= 0 || height <= 0)
+                return (NULL);
 
-	a = malloc(sizeof(int *)* height);
+        a = malloc(sizeof(int *) * height);
 
         if (a == NULL)
         {
                 free(a);
-		return (NULL);
-	}
-	for (i = 0; i > height; i++)
-	{
-			a[i] = malloc(sizeof(int) * width);
+                return (NULL);
+        }
+        for (i = 0; i < height; i++)
+        {
+                        a[i] = malloc(sizeof(int) * width);
 
-	if (a[i] = = NULL)
-	{
-		for (j = i; j >= 0; j--)
-		{
-			free(a[j]);
-		}
-		free(a);
-		return (NULL);
-	}
-	}
+                        if (a[i] == NULL)
+                        {
+                                for (j = i; j >= 0; j--)
+                                {
+                                        free(a[j]);
+                                }
+                                free(a);
+                                return (NULL);
+                        }
+        }
 
-	for (k = 0; k < width; l++)
-	{
-		for (l = 0; l < width; l++)
-		{
-			a[k][l] = 0;
-		}
-	}
-		return (a);
+        for (k = 0; k < width; l++)
+        {
+                for (l = 0; l < width; l++)
+                {
+                        a[k][l] = 0;
+                }
+        }
+        return (a);
 }
