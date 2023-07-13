3a1746f75a2c63437cc3c5bd02e7ac734fc3896c
hakima7 <hakimakarboub@gmail.com>
2023-07-13 16:05:50 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1];
 	s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) ||!is_digit(s2))
+	if (argc != 3 || !is_digit(argv[1]) ||!is_digit(argv[2]))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
