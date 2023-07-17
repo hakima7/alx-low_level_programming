f18dd6dd65829c0ccc2d1aa511da66f57c84ca33
hakima7 <hakimakarboub@gmail.com>
2023-07-17 09:54:01 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the fole program was compiled from
+ * Return: Alwayas 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
