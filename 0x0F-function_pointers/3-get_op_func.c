6315f511ad96ca88eb91aa447b2e7090dcf1a6dc
hakima7 <hakimakarboub@gmail.com>
2023-07-19 09:37:21 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,7 +11,7 @@
 int (*get_op_func(char*s))(int, int)
 {
         (void) s;
-        op_t op[s] = {
+        op_t ops[] = {
                 {"+", op_add},
                 {"-", op_sub},
                 {"*", op_mul},
