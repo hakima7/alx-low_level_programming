79cc2997430cc68ccb19f55852b4fecd5456312f
hakima7 <hakimakarboub@gmail.com>
2023-08-07 02:57:26 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,8 +5,8 @@
  * @filename: Apointer to the name of the file to create
  * @text_content: A pointer to a string to write to the file
  *
- * Return: if the finction fails --1
- * otherwise -1
+ * Return: if the finction fails - - 1
+ * otherwise - 1
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1 || w == 1)
 		return (-1);
 
 	close(fd);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,11 +19,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-			len++
+			len++;
 	}
 
-	O = open(filename, O_WRONLY | O_APPEND);
-	W = write(o, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
