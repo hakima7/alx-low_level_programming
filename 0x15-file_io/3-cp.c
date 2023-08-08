bcbdcc4b12d99c9baded9b2c71b91171bb7d5b1b
hakima7 <hakimakarboub@gmail.com>
2023-08-07 17:42:05 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,31 +1,11 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
-
-/**
- * create_buffer - Allocates 1024 bytes for a buffer
- * @file: the name of the file buffer is storing chars for
- *
- * Return: Apointer to the newly-allocated buffer
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-				"Error: Can't Write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
 
 /**
  * close_file - closes file descriptors
@@ -42,7 +22,6 @@ void close_file(int fd)
 		exit(100);
 	}
 }
-
 /**
  * main - copies the contents of a file to another file
  * @argc: the number of arguments supplied to the proram
@@ -68,29 +47,42 @@ int main(int argc, char *argv[])
 
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+		free(buffer);
+		close_file(from);
+		exit(99);
+	}
 
 	do {
-		if (from == -1 || r == -1)
+		r = read(from, buffer, 1024);
+		if (r == -1)
 		{
-			dprintf(STDERR_FILENO,
-					"Error: can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 			free(buffer);
+			close_file(from);
+			close_file(to);
 			exit(98);
 		}
 
 		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		if (w == -1)
 		{
-			dprintf(STDERR_FILENO,
-					"Erroe: can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
 			free(buffer);
+			close_file(from);
+			close_file(to);
 			exit(99);
 		}
-
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
@@ -100,4 +92,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
