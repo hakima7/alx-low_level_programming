8ca0de3c9ffbe133ea6123694a6f0882fc4d94f5
hakima7 <hakimakarboub@gmail.com>
2023-08-07 18:21:19 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,31 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer
+ * @file: the name of the file buffer is storing chars for
+ *
+ * Return: Apointer to the newly-allocated buffer
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+        {
+		dprintf(STDERR_FILENO,
+                                "Error: Can't Write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
 
 /**
  * close_file - closes file descriptors
@@ -14,14 +34,14 @@ void close_file(int fd);
 void close_file(int fd)
 {
 	int c;
-
-	c = close(fd);
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
-		exit(100);
-	}
+        c = close(fd);
+        if (c == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
+                exit(100);
+        }
 }
+
 /**
  * main - copies the contents of a file to another file
  * @argc: the number of arguments supplied to the proram
@@ -37,7 +57,7 @@ void close_file(int fd)
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
-	char *buffer;
+        char *buffer;
 
 	if (argc != 3)
 	{
@@ -46,49 +66,35 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	if (from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-		free(buffer);
-		exit(98);
-	}
-
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		free(buffer);
-		close_file(from);
-		exit(99);
-	}
+        from = open(argv[1], O_RDONLY);
+        r = read(from, buffer, 1024);
+        to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		r = read(from, buffer, 1024);
-		if (r == -1)
+		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-			free(buffer);
-			close_file(from);
-			close_file(to);
-			exit(98);
-		}
+			dprintf(STDERR_FILENO,
+                                        "Error: can't read from file %s\n", argv[1]);
+                        free(buffer);
+                        exit(98);
+                }
 
 		w = write(to, buffer, r);
-		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+		if (to == -1 || w == -1)
+                {
+			dprintf(STDERR_FILENO,
+					"Erroe: can't write to %s\n", argv[2]);
 			free(buffer);
-			close_file(from);
-			close_file(to);
 			exit(99);
 		}
 
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+		
 	} while (r > 0);
 
-	free(buffer);
-	close_file(from);
+        free(buffer);
+        close_file(from);
 	close_file(to);
-
-	return (0);
+        return (0);
 }
