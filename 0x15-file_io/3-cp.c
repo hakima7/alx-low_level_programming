ad6565fd7ae06e807aa251a8390449891fca3ae6
hakima7 <hakimakarboub@gmail.com>
2023-08-09 15:17:55 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,8 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't Write to %s\n", file);
+		dprintf(STDERR_FILENO,
+			"Error: Can't Write to %s\n", file);
 		exit(99);
 	}
 
@@ -74,7 +75,8 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO,
+				"Error: can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
